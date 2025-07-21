3660e287f454ac03d6b47ca22e91b435676a0061
Sriram Nidamanuri <96643997+nidamanurisriram@users.noreply.github.com>
2025-07-21 19:07:26 +0530

    Update RuntimeInputDemo.java

--- basic/fundamental/RuntimeInputDemo.java
+++ basic/fundamental/RuntimeInputDemo.java
@@ -10,44 +10,48 @@ import java.util.*;
 
 public class RuntimeInputDemo {
 	
-	private static Scanner scanner;
-	
 	public void numberRunTimeInput() {
 		System.out.print("Please enter a number : ");
+		Scanner scanner = new Scanner(System.in);
 		int number = scanner.nextInt();
 		System.out.println("Runtime input number : " + number);
 	}
 	
 	public void decimalRunTimeInput() {
 		System.out.print("Please eneter a decimal : ");
+		Scanner scanner = new Scanner(System.in);
 		double decimal = scanner.nextDouble();
 		System.out.println("Runtime input decimal : " + decimal);
 	}
 	
 	public void charRunTimeInput() {
 		System.out.print("Please enter a character : ");
+		Scanner scanner = new Scanner(System.in);
 		char character = scanner.next().charAt(0);
 		System.out.println("Runtime input character : " + character);
 	}
 	
 	public void booleanRunTimeInput() {
 		System.out.print("Please enter a boolean : ");
+		Scanner scanner = new Scanner(System.in);
 		boolean status = scanner.nextBoolean();
 		System.out.println("Runtime input boolean : " + status);
 	}
 	
 	public void stringRunTimeInput() {
-		System.out.print("Please enter a string : ");
-		String str1 = scanner.next();
-		String str2 = scanner.nextLine(); 
-		System.out.println("Runtime input string : " + str1 + " " + str2);
+		System.out.print("Please enter a single word string : ");
+		Scanner scanner1 = new Scanner(System.in);
+		String str1 = scanner1.next();
+		System.out.println("Runtime input string : " + str1);
+		System.out.print("Please enter a sentense(single or multiple word) string : ");
+		Scanner scanner2 = new Scanner(System.in);
+		String str2 = scanner2.nextLine();
+		System.out.print("Runtime input string : " + str2);
 	}
 	
 	public static void main(String [] args) {
 		
 		RuntimeInputDemo runtimeInputDemo = new RuntimeInputDemo();
-		
-		scanner = new Scanner(System.in);
 		
 		runtimeInputDemo.numberRunTimeInput();
 		
