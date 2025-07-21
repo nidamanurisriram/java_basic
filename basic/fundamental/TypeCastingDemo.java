6daf74e48006c36faabfa527bdc9a2feccfa0ed1
Sriram Nidamanuri <96643997+nidamanurisriram@users.noreply.github.com>
2025-07-21 19:04:51 +0530

    Update TypeCastingDemo.java

--- basic/fundamental/TypeCastingDemo.java
+++ basic/fundamental/TypeCastingDemo.java
@@ -1,4 +1,4 @@
-// Casting one data type to another data type or casting one object to another object is casting.
+// Casting one data type to another data type or casting one object to another object is type casting.
 
 // Converting one data type another data type is data type casting.
 
@@ -18,21 +18,22 @@
 
 // Upcasting syntax : ParentClassName parentClassNameReferenceVariable = new ChildClassName();
 
-// Upcasting is used to access parent class program members with child class implementation.
+// Upcasting is used to access child non static class program members through parent class object. 
 
-// This parent class reference variable will be consisting its program members from the child class constructor. Casting this parent class object to child class to get the child class properties is referred to as downcasting. To perform downcasting upcasting is manadatory and upcasted object has to be casted to child class for downcasting.
+// If parent and child classes both sharing same program members then after upcasting these program members of parent class will have the child class proerties   
+
+// Casting this parent class object to child class to get the child class properties is referred to as downcasting. To perform downcasting upcasting is manadatory and upcasted object has to be casted to child class for downcasting
 
 // Downcasting syntax : ChildClassName childClassNameReferenceVariable = (ChildClassName) parentClassReferenceVariable;
 
-// Downcasting is used to get the all the child class properties from the upcasted parent class object.
+// Downcasting is used to get the all the child class properties to its object from the upcasted parent class object.
 
 package fundamental;
-
 
 class First {
 	
 	public void print() {
-		System.out.println("First class method.");
+		System.out.println("First class method");
 	}
 	
 }
@@ -41,36 +42,71 @@ class Second extends First {
 	
 	@Override
 	public void print() {
-		System.out.println("Second class method.");
-	}
-	
-	public String print(String print) {
-		return print;
+		System.out.println("Second class method");
 	}
 	
 }
 
 
 public class TypeCastingDemo {
-	
-	
+		
 	public void wideningCasting() {
+		
+		char ch = 'A';
+		
+		int cno =  ch;
+		
+		System.out.println("Character input data " + ch);
+		
+		System.out.println("Character to Integer converted data " + cno);
 		
 		int number = 10;
 		
 		double decimal = number;
 		
-		System.out.println("Small data type value can be stored direct into large data types : " + decimal);
+		System.out.println("Integer input data " + number);
+		
+		System.out.println("Integer to double converted data " + decimal);
 		
 	}
 	
 	public void narrowingCasting() {
 		
-		double decimal = 10.01;
+		int no = 75;
+		
+		char ch = (char) no;
+		
+		System.out.println("Integer input data " + no);
+		
+		System.out.println("Integer to charavter converted data " + ch);
+		
+		double decimal = 10.14;
 		
 		int number = (int) decimal;
 		
-		System.out.println("To save large data types data into small data types the data has to be casted into storing data type : " + number);
+		System.out.println("Double input data " + decimal);
+		
+		System.out.println("Double to integer converted data " + number);
+		
+	}
+	
+	public void stringConversion() {
+		
+		String sno = "123";
+		
+		int number = Integer.parseInt(sno);  // converting string to primitive
+		
+		System.out.println("String input data " + sno);
+		
+		System.out.println("String to integer converted data " + number);
+		
+		boolean result = false; 
+		
+		String status = String.valueOf(result); // converting primitive to string		
+		
+		System.out.println("Boolean input data " + result);
+		
+		System.out.println("Boolean to string converted data " + status);
 	}
 	
 	public static void main(String[] args) {
@@ -81,22 +117,28 @@ public class TypeCastingDemo {
 		
 		typeCastingDemo.narrowingCasting();
 		
+		typeCastingDemo.stringConversion();
+		
 		System.out.println("Upcasting and Downcasting example");
 		
 		First first = new First();
+		
+		System.out.print("Parent class print method output: " );
 		
 		first.print();
 		
 		First first1 = new Second();   // Casting child class constructor to parent class object - upcasting.
 		
+		System.out.print("Parent class print method output after upcastinhg: " );
+		
 		first1.print();
 		
 		Second second = (Second) first1; // Converting parent class object to child class object - Doowncasting.
 		
+		System.out.print("Child class print method output after downcasting: ");
+		
 		second.print();
-		
-		System.out.println(second.print("Second class overloaded print method."));
-		
+				
 	}
 
 }
